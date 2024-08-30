e5f082f6b5640b2ca2ee22ad5618da90bf556ddb
Hongyuan Zhang <66273343+Alias-z@users.noreply.github.com>
2024-08-30 11:30:10 +0200

    Added peft_optimizer

--- mmseg/engine/optimizers/__init__.py
+++ mmseg/engine/optimizers/__init__.py
@@ -2,8 +2,9 @@
 from .force_default_constructor import ForceDefaultOptimWrapperConstructor
 from .layer_decay_optimizer_constructor import (
     LayerDecayOptimizerConstructor, LearningRateDecayOptimizerConstructor)
+from .peft_optimizer import PEFTOptimWrapperConstructor
 
 __all__ = [
     'LearningRateDecayOptimizerConstructor', 'LayerDecayOptimizerConstructor',
-    'ForceDefaultOptimWrapperConstructor'
+    'ForceDefaultOptimWrapperConstructor', 'PEFTOptimWrapperConstructor'
 ]
