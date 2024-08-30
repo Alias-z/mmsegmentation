8083f7c62e82b00d9047af0822c81c3270b3e812
Hongyuan Zhang <66273343+Alias-z@users.noreply.github.com>
2024-08-30 12:55:11 +0200

    Updated stomata_dataset

--- mmseg/datasets/stomata_dataset.py
+++ mmseg/datasets/stomata_dataset.py
@@ -3,15 +3,8 @@ from mmseg.registry import DATASETS
 from mmseg.datasets import BaseSegDataset
 from mmseg.utils import register_all_modules
 
-# classes = ('background', 'stomatal complex', 'stoma', 'outer ledge', 'pore', 'pavement cell', 'guard cell')
-# palette = [[0, 0, 0], [170, 0, 0], [231, 116, 237], [255, 245, 54], [234, 234, 235], [255, 170, 0], [85, 85, 255]]
-
-# classes = ('background', 'stomatal complex', 'stoma')
-# palette = [[0, 0, 0], [170, 0, 0], [231, 116, 237]]
-
-classes = ('background', 'stomatal complex')
-palette = [[0, 0, 0], [170, 0, 0]]
-
+classes = ('background', 'stomatal complex', 'stoma', 'outer ledge', 'pore', 'pavement cell', 'guard cell')
+palette = [[0, 0, 0], [170, 0, 0], [231, 116, 237], [255, 245, 54], [234, 234, 235], [255, 170, 0], [85, 85, 255]]
 
 @DATASETS.register_module()
 class StomataDataset(BaseSegDataset):
